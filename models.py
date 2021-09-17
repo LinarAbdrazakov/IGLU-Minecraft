e1847c2ed8dbffb46e0dfede3ed1c28becbf414f
gscraid <igor.churin19@gmail.com>
2021-09-17 23:34:44 +0400

    updated scripts

--- models.py
+++ models.py
@@ -7,7 +7,7 @@ import torch.nn as nn
 
 class VisualEncoder(nn.Module):
     def __init__(self, features_dim=512):
-        super(VisualEncoder, self).__init__()
+        super().__init__()
 
         self.cnn = nn.Sequential(
             nn.Conv2d(3, 32, kernel_size=8, stride=4, padding=0),  # (3, 64, 64) -> (32, 14, 14)
@@ -25,28 +25,22 @@ class VisualEncoder(nn.Module):
     def forward(self, x):
         return self.linear(self.cnn(x))
 
-
 class VisualDecoder(nn.Module):
     def __init__(self, features_dim=512):
-        super(VisualDecoder, self).__init__()
+        super().__init__()
 
         n_flatten = 1024
         self.linear = nn.Sequential(nn.Linear(features_dim, n_flatten), nn.ReLU())
 
         self.cnn = nn.Sequential(
-            nn.ConvTranspose2d(64, 64, ),
+            nn.ConvTranspose2d(64, 64, kernel_size=3,stride=1, padding=0),
             nn.ReLU(),
-            nn.ConvTranspose2d(),
+            nn.ConvTranspose2d(64, 32, kernel_size=4, stride=2, padding=0,output_padding=1),
+            # nn.ConvTranspose2d(64, 32, kernel_size=4, stride=2, padding=0),
             nn.ReLU(),
-            nn.ConvTranspose2d(),
+            nn.ConvTranspose2d(32, 3, kernel_size=8,stride=4, padding=0),
             nn.Tanh()
         )
-    
-    def forward(self, x):
-        x = self.linear(x)
-        x = x.reshape(x.shape[0], 64, 3, 3)
-        x = self.cnn(x)
-        return x 
 
 # Target Autoencoder Parts
 
